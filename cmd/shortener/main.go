7c15f73f9d65fc54406385854fe33b809f0fefa1
n1l <dhvega@yandex.ru>
2023-12-12 23:09:26 +0300

    Create first version of url shortener service

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,76 @@
 package main
 
-func main() {}
+import (
+	"crypto/md5"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+var shortedUrls map[string]string = make(map[string]string)
+
+func getHashOfUrl(url string) string {
+	sum := md5.Sum([]byte(url))
+	encoded := base64.StdEncoding.EncodeToString(sum[:])
+
+	return encoded[:8]
+}
+
+func CreateShortedUrlHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	if r.Method != http.MethodPost {
+		http.Error(w, "Bad Request!", http.StatusBadRequest)
+		return
+	}
+
+	body, readErr := io.ReadAll(r.Body)
+	if readErr != nil {
+		http.Error(w, "Bad Request!", http.StatusBadRequest)
+		return
+	}
+
+	stringUri := string(body)
+	if _, parseErr := url.ParseRequestURI(stringUri); parseErr != nil {
+		http.Error(w, "Bad Request!", http.StatusBadRequest)
+		return
+	}
+
+	hashId := getHashOfUrl(stringUri)
+	shortedUrls[hashId] = stringUri
+	resultStr := fmt.Sprintf("http://%s/%s\n", r.Host, hashId)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(resultStr))
+}
+
+func GetUrlByHash(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Bad Request!", http.StatusBadRequest)
+		return
+	}
+
+	hashId := chi.URLParam(r, "id")
+	if url, ok := shortedUrls[hashId]; ok {
+		http.Redirect(w, r, url, http.StatusPermanentRedirect)
+		return
+	}
+
+	http.Error(w, "Bad Request!", http.StatusBadRequest)
+}
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Post("/", CreateShortedUrlHandler)
+	router.Get("/{id}", GetUrlByHash)
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/n1l/url-shortener
 
 go 1.21.4
+
+require github.com/go-chi/chi/v5 v5.0.10
