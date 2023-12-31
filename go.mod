dcb651f2d2ec3ead5c2020b40e3aedd0313b6a15
n1l <dhvega@yandex.ru>
2023-12-31 20:00:53 +0300

    Add environment variables configuration

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21.4
 require github.com/go-chi/chi/v5 v5.0.10
 
 require (
+	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/httplog/v2 v2.0.8
 	github.com/stretchr/testify v1.8.4
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,15 +2,23 @@ package config
 
 import (
 	"flag"
+	"log"
+
+	"github.com/caarlos0/env/v6"
 )
 
 var Options struct {
-	PrivateHost string
-	PublicHost  string
+	PrivateHost string `env:"SERVER_ADDRESS"`
+	PublicHost  string `env:"BASE_URL"`
 }
 
 func ParseOptions() {
 	flag.StringVar(&Options.PrivateHost, "a", "localhost:8080", "The service address at start")
 	flag.StringVar(&Options.PublicHost, "b", "localhost:8080", "The shortener result base address")
 	flag.Parse()
+
+	err := env.Parse(&Options)
+	if err != nil {
+		log.Print("Failed to read environment variables")
+	}
 }
